f5b62e6dda06d5237241b5a6a7202d1e4fea4d1b
Isaac Henry <isaachenry751@gmail.com>
2023-11-08 08:26:42 -0500

    commited

--- frontend/index.js
+++ frontend/index.js
@@ -15,6 +15,37 @@ async function moduleProject4() {
   ]
 
   // 👉 Tasks 1 - 5 go here
+// Task 1
+document.getElementById('weatherWidget').style.display = 'none';
+
+const cityDropdown = document.getElementById('cityDropdown');
+
+// Task 2 and Task 3
+cityDropdown.addEventListener('change', (event) => {
+  // Task 3
+  cityDropdown.disabled = true;
+  document.getElementById('weatherWidget').style.display = 'none';
+  document.querySelector('p.info').textContent = 'Fetching weather data...';
+
+  // Task 4
+  axios.get(`http://localhost:3003/api/weather?city=${encodeURIComponent(event.target.value)}`)
+    .then(response => {
+      // Task 5
+      const weatherData = response.data;
+      document.querySelector('p.info').textContent = '';
+      cityDropdown.disabled = false;
+      document.getElementById('weatherWidget').style.display = 'block';
+
+      document.getElementById('cityName').textContent = weatherData.name;
+      document.getElementById('temperature').textContent = `${weatherData.temperature}°`;
+      const weatherDescriptionEmoji = descriptions.find(desc => desc[0] === weatherData.weather_description)[1];
+      document.getElementById('weatherDescription').textContent = weatherDescriptionEmoji;
+    })
+    .catch(error => {
+      // Task 4 (Error Handling)
+      console.error(error.message);
+    });
+});
 
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
